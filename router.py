de65dc283c0974e9f0ca745b56b931234725a226
tanna-1 <tan.nayir@student.ucll.be>
2024-10-02 12:23:47 +0200

    Added 404 and JSON page to DebugRouter

--- router.py
+++ router.py
@@ -2,6 +2,7 @@ from http11.request import HTTPRequest
 from http11.response import HTTPResponse
 from networking.address import TCPAddress
 from abc import ABC
+import json
 
 
 class Router(ABC):
@@ -10,6 +11,40 @@ class Router(ABC):
 
 class DebugRouter(Router):
     def handle(self, requester: TCPAddress, request: HTTPRequest) -> HTTPResponse:
+        if request.url in self.HANDLERS:
+            return self.HANDLERS[request.url](self, requester, request)
+
+        return HTTPResponse(
+            404,
+            {
+                "Content-Type": "text/html; charset=utf-8",
+                "X-Powered-By": "Tan's HTTP Server",
+            },
+            "404 Not Found".encode("utf-8"),
+        )
+
+    def json_page(self, requester: TCPAddress, request: HTTPRequest) -> HTTPResponse:
+        content = {
+            "requester": str(requester),
+            "request": {
+                "headers": request.headers,
+                "url": request.url,
+                "method": request.method,
+                "version": request.version,
+                "body_ascii": request.body.decode("ascii", "ignore"),
+            },
+        }
+
+        return HTTPResponse(
+            200,
+            {
+                "Content-Type": "application/json; charset=utf-8",
+                "X-Powered-By": "Tan's HTTP Server",
+            },
+            json.dumps(content).encode("utf-8"),
+        )
+
+    def root_page(self, requester: TCPAddress, request: HTTPRequest) -> HTTPResponse:
         content = (
             f"<!DOCTYPE html><html><body><h3>Source Address</h3><p>{requester}</p>"
         )
@@ -28,3 +63,5 @@ class DebugRouter(Router):
             },
             content.encode("utf-8"),
         )
+
+    HANDLERS = {"/": root_page, "/json": json_page}

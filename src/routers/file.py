122aa88fc1af9dbbd46990d486216066d38d601f
tanna-1 <tan.nayir@student.ucll.be>
2024-10-03 21:47:11 +0200

    Added strict symlink checks to FileRouter, symlinks...
    are disabled by default and won't be followed unless enabled.

--- src/routers/file.py
+++ src/routers/file.py
@@ -22,7 +22,20 @@ class FileRouter(Router):
         document_root: str | os.PathLike,
         generate_index: bool = True,
         generate_etag: bool = True,
+        disable_symlinks: bool = True,
     ) -> None:
+        """Inits FileRouter.
+
+        Args:
+        document_root -- Document root.
+        generate_index -- If True, generated index pages will be served for directories.
+        generate_etag -- If True, ETag will be calculated and sent with every response.
+        disable_symlinks -- If True, symlinks won't be followed.
+
+        WARNING: Enabling symlinks may lead to unexpected results with authentication middlewares.
+        E.g. "/protected_folder" vs "/folder/../protected_folder"
+        """
+
         super().__init__(
             HTTPResponseFactory(
                 {
@@ -37,9 +50,16 @@ class FileRouter(Router):
         self.__document_root = Path(document_root).resolve()
         self.__generate_index = generate_index
         self.__generate_etag = generate_etag
+        self.__disable_symlinks = disable_symlinks
 
     def __is_path_allowed(self, path: Path):
-        return path.is_relative_to(self.__document_root)
+        resolved_path = path.resolve()
+        if self.__disable_symlinks:
+            if path != resolved_path:
+                return False
+
+        # Resolved path must be within document root
+        return resolved_path.is_relative_to(self.__document_root)
 
     def __get_content_type(self, path: Path) -> str:
         ret = "application/octet-stream"
@@ -50,35 +70,7 @@ class FileRouter(Router):
                 ret += f"; charset={encoding}"
         return ret
 
-    def _handle(self, requester: TCPAddress, request: HTTPRequest):
-        if request.method != "GET":
-            return 400
-
-        raw_path = urllib.parse.unquote(request.path.lstrip("/"))
-        full_path = self.__document_root.joinpath(raw_path).resolve()
-
-        # Prevent path traversal!
-        if not self.__is_path_allowed(full_path):
-            LOG.warning(f"Attempted path traversal! Returning 400.")
-            return 400
-
-        try:
-            if full_path.is_file():
-                return self.serve_file(request, full_path)
-            elif full_path.is_dir():
-                index_html = full_path.joinpath("index.html")
-                if index_html.is_file():
-                    return self.serve_file(request, index_html)
-                elif self.__generate_index:
-                    # Generate index if allowe and there is no index.html
-                    return self.serve_folder(full_path)
-        except Exception as exc:
-            LOG.exception("Error while accesing path", exc_info=exc)
-            return 500
-
-        return 404
-
-    def serve_file(self, request: HTTPRequest, path: Path):
+    def __serve_file(self, request: HTTPRequest, path: Path):
         with path.open("rb") as f:
             LOG.debug(f'Reading file "{path}"')
             f.seek(0, os.SEEK_END)
@@ -102,7 +94,7 @@ class FileRouter(Router):
             headers["Content-Type"] = self.__get_content_type(path)
             return HTTPResponse(200, headers, f.read(size))
 
-    def serve_folder(self, path: Path):
+    def __serve_folder(self, requester: TCPAddress, path: Path):
         # Turns any path into an absolute web path (relative to document root)
         def make_link(p: Path) -> str:
             rel_p = p.relative_to(self.__document_root)
@@ -112,8 +104,9 @@ class FileRouter(Router):
         def make_text(val):
             return html.escape(str(val))
 
-        # Display path relative to document root without quoting
-        content = f"<!DOCTYPE html><html><body><h3>{make_text(path.relative_to(self.__document_root))}</h3><ul>"
+        # Display path relative to document root
+        title = make_text(path.relative_to(self.__document_root).as_posix())
+        content = f"<!DOCTYPE html><html><head><title>{title}</title></head><body><h3>{title}</h3><ul>"
 
         # Display the parent path if possible
         if path.parent != path and self.__is_path_allowed(path.parent):
@@ -123,7 +116,36 @@ class FileRouter(Router):
         for sub_path in path.iterdir():
             content += f'<li><a href="{make_link(sub_path)}">{make_text(sub_path.name)}</a></li>'
 
-        content += (
-            f"</ul><p>Generated on {datetime.now().isoformat()}</p></body></html>"
-        )
+        content += f"</ul><p>Generated on {datetime.now().isoformat()} for {requester}</p></body></html>"
         return self.resp_factory.html(content)
+
+    def _handle(self, requester: TCPAddress, request: HTTPRequest):
+        if request.method != "GET":
+            return 400
+
+        # Unquote HTTP path, append it to document root
+        # The path is unsafe at this point
+        path = self.__document_root.joinpath(
+            urllib.parse.unquote(request.path.lstrip("/"))
+        )
+
+        # Prevent path traversal and optionally forbid symlinks
+        if not self.__is_path_allowed(path):
+            LOG.warning(f"Path not allowed: {path}")
+            return 403
+
+        try:
+            if path.is_file():
+                return self.__serve_file(request, path)
+            elif path.is_dir():
+                index_html = path.joinpath("index.html")
+                if index_html.is_file():
+                    return self.__serve_file(request, index_html)
+                elif self.__generate_index:
+                    # Generate index if allowe and there is no index.html
+                    return self.__serve_folder(requester, path)
+        except Exception as exc:
+            LOG.exception("Error while accesing path", exc_info=exc)
+            return 500
+
+        return 404

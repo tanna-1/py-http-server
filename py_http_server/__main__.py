e7a0778486729e74b970f28f6f4e1ab23f1c22a5
tanna-1 <tan.nayir@student.ucll.be>
2024-10-07 01:29:10 +0200

    Removed unnecessary check from __main__

--- py_http_server/__main__.py
+++ py_http_server/__main__.py
@@ -4,11 +4,10 @@ from .routers.file import FileRouter
 from .middlewares.compress import CompressMiddleware
 from .main import app_main
 
-if __name__ == "__main__":
-    app_main(
-        handler_chain=CompressMiddleware(DefaultMiddleware(FileRouter("."))),
-        http_listeners=[
-            TCPAddress("127.0.0.1", 80),
-            TCPAddress("::1", 80),
-        ],
-    )
+app_main(
+    handler_chain=CompressMiddleware(DefaultMiddleware(FileRouter("."))),
+    http_listeners=[
+        TCPAddress("127.0.0.1", 80),
+        TCPAddress("::1", 80),
+    ],
+)

94f2c6f2fc68a86a05ae1e71ecc4643758a0568a
tanna-1 <tan.nayir@student.ucll.be>
2024-10-07 00:10:24 +0200

    Removed auth from default module main

--- py_http_server/__main__.py
+++ py_http_server/__main__.py
@@ -1,17 +1,12 @@
 from .middlewares.default import DefaultMiddleware
 from .networking.address import TCPAddress
 from .routers.file import FileRouter
-from .middlewares.basic_auth import BasicAuthMiddleware
 from .middlewares.compress import CompressMiddleware
 from .main import app_main
 
 if __name__ == "__main__":
     app_main(
-        handler_chain=CompressMiddleware(
-            DefaultMiddleware(
-                BasicAuthMiddleware(FileRouter("."), credentials={"test": "test"})
-            )
-        ),
+        handler_chain=CompressMiddleware(DefaultMiddleware(FileRouter("."))),
         http_listeners=[
             TCPAddress("127.0.0.1", 80),
             TCPAddress("::1", 80),
